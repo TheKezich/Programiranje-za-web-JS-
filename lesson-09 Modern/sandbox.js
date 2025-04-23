8a967f57ef56ff27882c1089674a95bcbe979991
Ivan Kezic <thekezich@gmail.com>
2025-04-23 11:01:33 +0200

    initial

---
+++ lesson-09 Modern/sandbox.js
@@ -0,0 +1,27 @@
+console.log('Hello world!')
+let index = email.indexOf('@') //na kojem je mjestu
+var score = 75;
+
+let email = 'thekezich@gmail.com';
+
+// let result = email.lastIndexOf('n')
+
+// let result = email.slice(2,5); //makne ta slova
+
+// let result = email.substring(2,3);
+
+// let result = email.replace('m', 'w')
+
+let radius = 10;
+const pi = 3.14;
+
+let likes = 23;
+//likes = likes +1
+likes++;
+likes--;
+
+likes += 10; //doda 10
+likes -= 5; //oduzme 5
+
+let result = 'the blog has' + likes + 'likes';
+
